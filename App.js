95574a6a9cc7c0cfcbeae7be71feaa0d1afb16ab
Rohan Damani <damanirohan@gmail.com>
2016-09-03 18:48:02 -0700

    Add Griddle

--- App.js
+++ App.js
@@ -1,53 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import Griddle from 'griddle-react'
+import object from './object'
+
 
 class App extends React.Component {
 		constructor () {
 			super ();
-			this.state = { 
-				red: 0,
-				green: 0,
-				blue: 0
-			}
-			this.update = this.update.bind(this)
-	}
-		update(e){
-			this.setState({
-				red: ReactDOM.findDOMNode(this.refs.red).value,
-				green: ReactDOM.findDOMNode(this.refs.green).value,
-				blue: ReactDOM.findDOMNode(this.refs.blue).value
-			})
 		}
 	render(){
 		return (
-			<div>
-			  <Slider ref="red" update={this.update} />
-			  {this.state.red}
-			  <br />
-			  <Slider ref="green" update={this.update} />
-			  {this.state.green}
-			  <br />
-			  <Slider ref="blue" update={this.update} />
-			  {this.state.blue}
-			  <br />
+			<div className="container">
+			  <h1 className="text-center"> Rohan's React News </h1>
+			  <Griddle results={object} showFilter={true} showSettings={true} />
 			</div>
+
 			);
 	}
 }
 
-class Slider extends React.Component {
-	render (){
-	return (
+// class Slider extends React.Component {
+// 	render (){
+// 	return (
 			
-			  <input type="range"
-			    min="0"
-			    max="255"
-			    onChange={this.props.update} />
+// 			  <input type="range"
+// 			    min="0"
+// 			    max="255"
+// 			    onChange={this.props.update} />
 			
-			);
+// 			);
 
-}
-}
+// 	}
+// }
 
 
 
---
+++ object.js
@@ -0,0 +1,85 @@
+let object =  [
+  {
+    "id": 0,
+    "name": "Mayer Leonard",
+    "city": "Kapowsin",
+    "state": "Hawaii",
+    "country": "United Kingdom",
+    "company": "Ovolo",
+    "favoriteNumber": 7
+  },
+    {
+    "id": 1,
+    "name": "Mayer Leonard",
+    "city": "Kapowsin",
+    "state": "Hawaii",
+    "country": "United Kingdom",
+    "company": "Ovolo",
+    "favoriteNumber": 7
+  },
+    {
+    "id": 2,
+    "name": "Mayer Leonard",
+    "city": "Kapowsin",
+    "state": "Hawaii",
+    "country": "United Kingdom",
+    "company": "Ovolo",
+    "favoriteNumber": 7
+  },
+    {
+    "id": 3,
+    "name": "Mayer Leonard",
+    "city": "Kapowsin",
+    "state": "Hawaii",
+    "country": "United Kingdom",
+    "company": "Ovolo",
+    "favoriteNumber": 7
+  },
+      {
+    "id": 4,
+    "name": "Mayer Leonard",
+    "city": "Kapowsin",
+    "state": "Hawaii",
+    "country": "United Kingdom",
+    "company": "Ovolo",
+    "favoriteNumber": 7
+  },
+      {
+    "id": 5,
+    "name": "Mayer Leonard",
+    "city": "Kapowsin",
+    "state": "Hawaii",
+    "country": "United Kingdom",
+    "company": "Ovolo",
+    "favoriteNumber": 7
+  },
+      {
+    "id": 6,
+    "name": "Mayer Leonard",
+    "city": "Kapowsin",
+    "state": "Hawaii",
+    "country": "United Kingdom",
+    "company": "Ovolo",
+    "favoriteNumber": 7
+  },
+      {
+    "id": 7,
+    "name": "Mayer Leonard",
+    "city": "Kapowsin",
+    "state": "Hawaii",
+    "country": "United Kingdom",
+    "company": "Ovolo",
+    "favoriteNumber": 7
+  },
+      {
+    "id": 8,
+    "name": "Mayer Leonard",
+    "city": "Kapowsin",
+    "state": "Hawaii",
+    "country": "United Kingdom",
+    "company": "Ovolo",
+    "favoriteNumber": 7
+  },
+];
+
+export default object
